6f5f28dd0d1d9103941843ed88d38d91f78ae18b
Josh Kaplan <jk@joshkaplan.org>
2015-02-24 21:48:22 -0500

    Arduino template

---
+++ Arduino_Practice/template/template.ino
@@ -0,0 +1,20 @@
+/**
+ * template.ino
+ *
+ * Josh Kaplan
+ *
+ * This is a template for your Arduino code.
+ */
+
+
+/* Setup runs once */
+void setup()
+{
+	// put setup code here
+}
+
+/* Loop runs infinitely */
+void loop()
+{
+	// put main loop code here
+}
